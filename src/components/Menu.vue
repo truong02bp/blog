c404a710f49173c0efb67a984f3cb0138f92bb8a
TruongNH <truong02bp@github.com>
2022-01-21 23:17:06 +0700

    first

---
+++ src/components/Menu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-for="(task, index) in tasks" :key = "index">
+      <input type="checkbox" v-model = "task.done"> {{ task.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Menu",
+  data() {
+    return {
+        tasks: [
+          {name : "ABC", done : true},
+          {name : "DEF", done : false},
+          {name : "XYZ", done : true},
+        ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
